48924e31cbb1b26310be1628375a86727d8b1623
이규정 <veracro97@gmail.com>
2026-04-07 02:26:14 +0900

    solve_Binary_Tree_Q8_E_BT

--- C_study/myfirstprogram.c
+++ C_study/myfirstprogram.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-int main()
-{
-    printf("Helloasdasd\n");
+int main() {
+    int arr[] = {0, 1, 2, 3};
+    int *ptr = arr;
+    printf("%d\n", *ptr++);
+    printf("%d\n", *ptr + 1);
+
     return 0;
 }
